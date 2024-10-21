dab089761765cbe2f4055f00348c6372f54386da
teffcode <teffcode@platzi.com>
2024-10-21 12:45:06 -0500

    feature(rendering patterns in react): add final project version

---
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -0,0 +1,18 @@
+import Tabs from "./Tabs"
+import Tab from "./Tab"
+
+export const CompoundComponents = () => {
+  return (
+    <Tabs>
+      <Tab label="Girafa">
+        🦒
+      </Tab>
+      <Tab label="Delfín">
+        🐬
+      </Tab>
+      <Tab label="Lagarto">
+        🦎
+      </Tab>
+    </Tabs>
+  );
+};
